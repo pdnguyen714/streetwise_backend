4f31475a30b0a012efb7c76e9d514637fe94873d
Bri Y <bri@bris-air.attlocal.net>
2020-06-22 14:33:42 -0700

    started on routes and database setup

---
+++ backend/routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+const config = require('../database')
+const { Client } = require('pg');
+
+router.get('/', async (req, res, next) => {
+  const client = new Client(config);
+  await client.connect();
+  const data = await client.query(`SELECT * FROM pins`)
+  console.log(data.rows)
+  await client.end()
+  res.json(data.rows)
+})
+
+router.post('/', async (req, res, next) => {
+  const client = new Client(config);
+  await client.connect();
+  const query = `
+    INSERT INTO pins (
+      longitude,
+      latitude,
+      title,
+      comment,
+      link
+    ) VALUES (
+      '${req.user.longitude}',
+      '${req.body.latitude}',
+      '${req.body.title}',
+      '${req.body.comment}',
+      '${req.body.link}'
+    );
+  `
+  const data = await client.query(query)
+  await client.end()
+  res.json(data)
+})
+
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+
+module.exports = router;
